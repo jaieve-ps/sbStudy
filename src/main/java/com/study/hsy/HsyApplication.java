f096c47221722db632328b8fe3f83464f1e67de2
jaieve-ps <jaieve1469@polestarhc.com>
2021-08-24 15:12:35 +0900

    reset

---
+++ src/main/java/com/study/hsy/HsyApplication.java
@@ -0,0 +1,13 @@
+package com.study.hsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/hsy/HsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.hsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
