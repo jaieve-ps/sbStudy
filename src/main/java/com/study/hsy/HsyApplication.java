72322552b1934dd1de120a1a514814936b51c709
jaieve <yoori0303@naver.com>
2021-08-24 23:39:31 +0900

    gitignore setting

--- src/main/java/com/study/hsy/HsyApplication.java
+++ src/main/java/com/study/hsy/HsyApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HsyApplication {
 
 	public static void main(String[] args) {
-
-
 		SpringApplication.run(HsyApplication.class, args);
 	}
 
